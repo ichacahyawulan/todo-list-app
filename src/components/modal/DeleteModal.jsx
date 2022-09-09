c4dde27fbf13e7206e727940db9414056af98434
Icha Cahya Wulan <ichacahyawulan@gmail.com>
2022-09-10 02:33:56 +0700

    create delete task modal

--- src/components/modal/DeleteModal.jsx
+++ src/components/modal/DeleteModal.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { HiOutlineExclamation } from 'react-icons/hi'
+import { IoClose } from 'react-icons/io5'
 
 import './assets/DeleteModal.css'
 
 export default function DeleteModal(props) {
     return (
         <div className='delete-modal'>
-            
+            <div className='head-body'>
+                <div className='head'>
+                    <div className='exclamation'>
+                        <HiOutlineExclamation size={24}></HiOutlineExclamation>
+                    </div>
+                    <div className='delete-title'>
+                        Delete Modal
+                    </div>
+                    <div className='close'>
+                        <IoClose size={24}></IoClose>
+                    </div>
+                </div>
+                <div className='body'>
+                Are you sure want to delete this task? your action can’t be reverted.
+                </div>
+            </div>
+            <div className='foot'>
+                <button className='cancel' onClick={() => alert("halo")}>
+                    <div className='text'>Cancel</div>
+                </button>
+                <button className='delete'>
+                    <div className='text'>Delete</div>
+                </button>
+            </div>
         </div>
     );
 }
