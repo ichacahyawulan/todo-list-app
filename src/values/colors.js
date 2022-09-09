4716c3b8b6fa522b14a6e531a0c9ab4747103637
Icha Cahya Wulan <ichacahyawulan@gmail.com>
2022-09-09 14:24:14 +0700

    define colors

---
+++ src/values/colors.js
@@ -0,0 +1,31 @@
+export const COLOR = {
+    primary: {
+        main: "#01959F",
+        border: "#4DB5BC",
+        surface: "#F7FEFF"
+    },
+    secondary: {
+        main: "#FA9810",
+        border: "#FEEABC",
+        surface: "#FFFCF5"
+    },
+    danger: {
+        main: "#E11428",
+        border: "#F5B1B7",
+        surface: "#FFFAFA"
+    },
+    succes: {
+        main: "#43936C",
+        border: "#B8DBCA",
+        surface: "#F8FBF9"
+    },
+    neutral: {
+        10: "#FFFFFF",
+        20: "#FAFAFA",
+        30: "#EDEDED",
+        40: "#E0E0E0",
+        70: "#757575",
+        90: "#404040",
+        100: "#1D1F20"
+    }
+}
